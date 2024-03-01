816c625eed7fe7e0ba14e693e1dc0e512afe97cb
Matos48 <mateusz.osojca@wp.pl>
2024-03-01 13:08:54 +0100

    errors handling in scraper

--- scraper.py
+++ scraper.py
@@ -1,9 +1,8 @@
 import time
-
+import re
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import re
 from bs4 import BeautifulSoup
 
 
@@ -23,56 +22,82 @@ class Scrapper:
         return driver
 
     def get_soup(self, url_address):
-        self.driver.get(url_address)
-        html = self.driver.page_source
-
-        soup = BeautifulSoup(html, "html.parser")
-
-        return soup
+        try:
+            self.driver.get(url_address)
+            html = self.driver.page_source
+            soup = BeautifulSoup(html, "html.parser")
+            return soup
+        except Exception as e:
+            print(f"Error fetching page content from {url_address}: {e}")
+        return None
 
     def get_newest_round(self, soup):
-        rounds = soup.find_all(class_="event__round--static")
-        newest_round = None
-        highest_round_number = -1  # Start with a default that's lower than any expected round number
+        try:
+            rounds = soup.find_all(class_="event__round--static")
+            if not rounds:
+                print("No rounds found.")
+                return []
 
-        for round in rounds:
-            round_number_text = round.text
-            # Use regular expression to find numbers in the round text
-            match = re.search(r'\d+', round_number_text)
-            if match:
-                round_number = int(match.group())
-                if round_number > highest_round_number:
-                    highest_round_number = round_number
-                    newest_round = round
+            newest_round = None
+            highest_round_number = 0
 
-        if newest_round:
-            matches = []
-            current_match = newest_round.find_next_sibling()
+            for round in rounds:
+                round_number_text = round.text
+                # Use regular expression to find numbers in the round text
+                match = re.search(r'\d+', round_number_text)
+                if match:
+                    round_number = int(match.group())
+                    if round_number > highest_round_number:
+                        highest_round_number = round_number
+                        newest_round = round
 
-            while current_match:
-                if 'class' in current_match.attrs:
-                    if 'event__match' in current_match['class']:
-                        matches.append(current_match)
+            if newest_round:
+                matches = []
+                current_match = newest_round.find_next_sibling()
 
-                    # It checks if the next round has been reached
-                    elif 'event__round' in current_match['class'] and 'event__round--static' in current_match['class']:
-                        break
+                while current_match:
+                    if 'class' in current_match.attrs:
+                        if 'event__match' in current_match['class']:
+                            matches.append(current_match)
 
-                current_match = current_match.find_next_sibling()
+                        # It checks if the next round has been reached
+                        elif 'event__round' in current_match['class'] and 'event__round--static' in current_match['class']:
+                            break
 
-            return matches
+                    current_match = current_match.find_next_sibling()
+
+                return matches
+        except Exception as e:
+            print(f"Error finding newest round: {e}")
+        return []
 
     def get_match_ids(self, matches):
+        try:
+            if not matches:
+                print("No matches provided to extract IDs from.")
+                return []
 
-        if matches:
             match_ids = [match['id'][4:] for match in matches if 'id' in match.attrs]
+            if not match_ids:
+                print("No valid match IDs found.")
             return match_ids
-    def get_odds_soup(self, match_ids):
 
-            if match_ids:
-                url = f"https://www.flashscore.pl/mecz/{match_ids[0]}/#/zestawienie-kursow/kursy-1x2/koniec-meczu"
-                print(url)
-                return self.get_soup(url)
+        except Exception as e:
+            print(f"Error extracting match IDs: {e}")
+            return []
+
+    def get_odds_and_players_urls(self, match_ids):
+        if not match_ids:
+            print("No match IDs provided for URL generation.")
+            return [], []
+
+        odds_url_list = []
+        players_url_list =[]
+        for match_id in match_ids:
+            odds_url_list.append(f"https://www.flashscore.pl/mecz/{match_id}/#/zestawienie-kursow/kursy-1x2/koniec-meczu")
+            players_url_list.append(f"https://www.flashscore.pl/mecz/{match_id}/#/szczegoly-meczu/sklady")
+
+        return odds_url_list, players_url_list
 
     def get_players_soup(self, match_ids):
 
@@ -90,8 +115,8 @@ matches = scraper.get_newest_round(main_soup)
 
 match_ids = scraper.get_match_ids(matches)
 time.sleep(10)
-odds_soup = scraper.get_odds_soup(match_ids)
+odds_soup = scraper.get_odds_and_players_urls(match_ids)
 time.sleep(10)
 players_soup = scraper.get_players_soup(match_ids)
-
+print(odds_soup)
 
